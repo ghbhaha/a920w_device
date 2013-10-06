2d2601228186305e30cd9270b7035ce3fdaefe11
ghbhaha <ghbhaha@gmail.com>
2013-10-06 18:08:07 +0800

    mkvendor.sh: Initial commit of A920W

---
+++ device_A920W.mk
@@ -0,0 +1,24 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/AMOI/A920W/A920W-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/AMOI/A920W/overlay
+
+LOCAL_PATH := device/AMOI/A920W
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_A920W
+PRODUCT_DEVICE := A920W
